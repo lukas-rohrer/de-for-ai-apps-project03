dd560c96bb52e7b0803f3f421800bcbbe9b9f3d0
Lukas Rohrer <l.rohrer@pm.me>
2022-08-25 17:46:38 +0200

    added cleanup script that deletes the redshift cluster and detaches the roles

---
+++ cleanup.py
@@ -0,0 +1,58 @@
+# goal: delete redshift cluster and detach roles
+import configparser
+import boto3
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    KEY                    = config.get('AWS','KEY')
+    SECRET                 = config.get('AWS','SECRET')
+    DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
+    DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
+
+
+
+    iam = boto3.client('iam',aws_access_key_id=KEY,
+                     aws_secret_access_key=SECRET,
+                     region_name='us-west-2'
+                  )
+
+    redshift = boto3.client('redshift',
+                        region_name="us-west-2",
+                        aws_access_key_id=KEY,
+                        aws_secret_access_key=SECRET
+                        )
+
+
+  
+    try:
+        print('Deleting Redshift cluster.')
+        redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
+
+        waiter = redshift.get_waiter('cluster_deleted')
+        waiter.wait(
+            ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+            MaxRecords=100,
+            WaiterConfig={
+                'Delay': 100,
+                'MaxAttempts': 100
+            }
+        )
+        print('Cluster successfully deleted.')
+
+    except Exception as e:
+        print(e)
+    
+
+    try:
+        print('Detatching roles.')
+        iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+        iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
+    except Exception as e:
+        print(e)
+
+    
+if __name__ == "__main__":
+    main()

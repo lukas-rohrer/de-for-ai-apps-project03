823957298daed557bf74e240cc95b0b4ddd2b03e
Lukas Rohrer <l.rohrer@pm.me>
2022-08-31 17:32:01 +0200

    fixed drop queries and serveral bugs in the staging queries

--- sql_queries.py
+++ sql_queries.py
@@ -7,17 +7,17 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = "DROP TABLE IF EXISTS staging_events_table_drop"
-staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs_table_drop"
-songplay_table_drop = "DROP TABLE IF EXISTS songplay_table_drop"
-user_table_drop = "DROP TABLE IF EXISTS user_table_drop"
-song_table_drop = "DROP TABLE IF EXISTS song_table_drop"
-artist_table_drop = "DROP TABLE IF EXISTS artist_table_drop"
-time_table_drop = "DROP TABLE IF EXISTS time_table_drop"
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-# registration also timestamp datatype?
+# registration also timestamp datatype? timestamp doesnt work, aws example uses double precision
 staging_events_table_create= ("""CREATE TABLE IF NOT EXISTS staging_events(
     artist varchar(255),
     auth varchar(255),
@@ -30,33 +30,33 @@ staging_events_table_create= ("""CREATE TABLE IF NOT EXISTS staging_events(
     location varchar(255), 
     method varchar(255),
     page varchar(255),
-    registration varchar(255), 
+    registration double precision, 
     sessionId int,
     song varchar(255),
     status int,
-    ts timestamp,
+    ts double precision,
     userAgent varchar(255),
     userId int
 )
 """)
 
 staging_songs_table_create = ("""CREATE TABLE IF NOT EXISTS staging_songs(
-    num_songs int,
-    artist_id varchar(255),
-    artist_latitude double precision,
-    artist_longitude double precision,
-    artist_location varchar(255),
-    artist_name	varchar(255),
     song_id	varchar(255),
+    num_songs int,
     title varchar(255),
+    artist_name	varchar(500),
+    artist_latitude double precision,
+    year int,
     duration double precision,
-    year int
+    artist_id varchar(255),
+    artist_longitude double precision,
+    artist_location varchar(500)
 )
 """)
 
 # postgres serial is not supported by redshift -> use IDENTITY(0,1)
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(
-    songplay_id IDENTITY(0,1) primary key,
+    songplay_id int IDENTITY(0,1) primary key,
     start_time timestamp references time(start_time) NOT NULL,
     user_id integer references users(user_id) NOT NULL,
     level varchar(255),
@@ -111,13 +111,12 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time(
 
 staging_events_copy = (
     """
-    copy staging events
-    from '{}'
+    copy staging_events
+    from {}
     iam_role '{}'
     json {}
-    compupdate off
     region 'us-west-2'
-    """).format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'])
+    """).format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'], config['S3']['LOG_JSONPATH'])
 
 
 """
@@ -130,8 +129,14 @@ region 'us-west-2';
 """
 
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = (
+    """
+    copy staging_songs
+    from {}
+    iam_role '{}'
+    region 'us-west-2'
+    format as json 'auto'
+""").format(config['S3']['SONG_DATA'], config['IAM_ROLE']['ARN'])
 
 # FINAL TABLES
 
@@ -152,7 +157,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]

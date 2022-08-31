3fb25172e9ed183b780f5a94217d4b67ed89e63e
Lukas Rohrer <l.rohrer@pm.me>
2022-08-31 17:33:49 +0200

    adjusted db connection paramaters

--- etl.py
+++ etl.py
@@ -19,11 +19,15 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    print("Connecting to database.")
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(config["DWH"]["DWH_ENDPOINT"], config["DWH"]["DWH_DB"], config["DWH"]["DWH_DB_USER"], config["DWH"]["DWH_DB_PASSWORD"], config["DWH"]["DWH_PORT"]))
+    print("Succesfully connected to database.")
     cur = conn.cursor()
     
+    print("Copying the data into the staging tables.")
     load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+
+    # insert_tables(cur, conn)
 
     conn.close()
 
